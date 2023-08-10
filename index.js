9ede9f85f5149c696145756b175d14e6c8bf89f8
Aleksander Kurgan <aleksanderkurgan9@gmail.com>
2023-08-09 20:20:48 -0400

    test

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const product = require('./api/product');
-require('dotenv').config(); // Load environment variables
-
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.use(express.static('public'));
-
-// Route to get API key
 app.get('/api/key', (req, res) => {
   res.json({ api_key: process.env.CLIENT_ID || '' });
 });
 
-app.use("/api/product", product);
-
-const PORT = process.env.PORT || 5050;
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
