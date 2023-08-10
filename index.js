1da6ebf2bf7a5ba85a89eeb9c77e9ca63c52ebf0
Aleksander Kurgan <aleksanderkurgan9@gmail.com>
2023-08-09 21:17:21 -0400

    test

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const path = require('path'); // Add this line
+const path = require('path');
 dotenv.config();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/key', (req, res) => {
   res.json({ api_key: process.env.CLIENT_ID || '' });
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
