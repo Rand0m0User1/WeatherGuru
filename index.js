7d0a14c7a8344e26af03203104c5db2d2dc0934c
Aleksander Kurgan <aleksanderkurgan9@gmail.com>
2023-08-09 21:08:31 -0400

    test

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const path = require('path'); // Add this line
+const path = require('path');
 dotenv.config();
 
-// Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/key', (req, res) => {
   res.json({ api_key: process.env.CLIENT_ID || '' });
 });
 
-// Catch-all route for handling frontend routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
